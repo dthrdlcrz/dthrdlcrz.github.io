76eb01f45a12c5275c3509cf590b2663c6c9465f
Diether Dela Cruz <dgdelacruz@powersolv-ph.com>
2026-05-05 12:38:13 +0800

    Clean repo for GitHub Pages

--- Program.cs
+++
@@ -1,7 +0,0 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
-
-app.UseDefaultFiles();  // serves index.html at /
-app.UseStaticFiles();   // serves css, js, images
-
-app.Run();
