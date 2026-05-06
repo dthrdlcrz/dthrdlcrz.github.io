a69782b15ea95ac5a5e89bd30339ba642950c42b
Diether Dela Cruz <62082694+dthrdlcrz@users.noreply.github.com>
2026-05-06 08:17:18 +0800

    Delete ClientApp directory

--- ClientApp/src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import styles from './Footer.module.css'
-
-export default function Footer() {
-  return (
-    <footer className={styles.footer}>
-      Designed & Built by Diether Garcia Dela Cruz · {new Date().getFullYear()}
-    </footer>
-  )
-}
